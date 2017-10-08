2252988d49c65d2a624c3653351dcc52766007fe
nullmasterr <32000879+nullmasterr@users.noreply.github.com>
2017-10-08 16:46:47 +0300

    Add files via upload

---
+++ randompassgen_p.py
@@ -0,0 +1,74 @@
+# DevOn Random Password Generator v1.0 [Public]
+
+"""
+		This is the public version of DevOn Random Password Generator
+		
+		You can copy and use the outline of this version.
+		
+		To contact us for anything, really anything.
+		
+		Just mail us nullmaster@yahoo.com
+	
+"""
+
+
+import random
+import ...
+
+def rpg():
+
+	print("Welcome to the Null's password generator v1.0")
+	print ...
+	
+	def savepw(): 		# Saves current password.
+		global pwlist
+		global ...		
+	
+	def showpw():		# Show the current password to user.
+		try:
+			print("====================================================================")
+			print ...
+			
+	def init():			# Initial Conditions
+	
+		print("Set the initial conditions.")
+		print ...
+		
+	
+	def spcedit():		# Editing Special Characters. (Add/Remove)
+		
+		print("Type 'DONE' to save and quit editting.")
+		print ...
+	
+	def showhelp():		# Shows all commands that generator has.
+			print("====================================================================")
+			print ...
+			
+	def spc():		# Show user existing special characters.
+		print("These are the existing special characters.")
+		return print ...
+			
+	def conditions():		# Used to re-build password conditions.
+			
+		print("Reseting conditions.")
+		
+		global ...
+	
+	
+	def generate():
+			
+		global pw
+	
+		pw = ""
+		
+		...
+		...
+			
+			
+			
+			
+	while True:
+	
+		get_input = input("...")
+		
+		if ...
